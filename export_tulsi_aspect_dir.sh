8838559867ef48ece205992199d2732e92aa34a2
Jerry Marino <i@jerrymarino.com>
2019-01-18 14:33:05 -0800

    Remove unused BUILD file

--- export_tulsi_aspect_dir.sh
+++ export_tulsi_aspect_dir.sh
@@ -20,6 +20,7 @@ ditto $TULSI_DIR/src/TulsiGenerator/Bazel/* $EXPORT_DIR/tulsi/
 mv $EXPORT_DIR/tulsi/WORKSPACE $EXPORT_DIR
 
 ditto $TULSI_DIR/src/TulsiGenerator/Scripts/* $EXPORT_DIR
+rm -rf $EXPORT_DIR/BUILD
 
 # Cleanup unused test files
 for f in $(find $EXPORT_DIR -name *tests\.py);  do rm -rf $f; done
